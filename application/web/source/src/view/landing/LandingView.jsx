4ff7063b7e3f5b635cfe7e6f8735429361795146
Thomas Davis <tldavis09@yahoo.com>
2023-04-05 11:52:31 -0500

    WA: Implement navigation from landing to demographics view

--- application/web/source/src/view/landing/LandingView.jsx
+++ application/web/source/src/view/landing/LandingView.jsx
@@ -5,15 +5,22 @@ import { useNavigate } from "react-router-dom";
 function LandingView() {
 	const navigate = useNavigate();
 
-	const navToLogin = () => {
+	function navToLogin() {
 		navigate("/login");
-	};
+	}
+
+	function navToDemographics() {
+		navigate("/demographics");
+	}
 
 	return (
 		<div id="landing-view">
 			<TitleRegion title="Home" />
 			<button id="nav-to-login" type="button" onClick={navToLogin}>
 				Login
+			</button>
+			<button id="nav-to-demographics" type="button" onClick={navToDemographics}>
+				Guest/Demographics/Whatever
 			</button>
 		</div>
 	);
