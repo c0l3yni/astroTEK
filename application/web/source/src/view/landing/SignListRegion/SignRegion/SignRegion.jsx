4ffd302517c91b40ad3ab8e0789ee4e995c656ac
Thomas Davis <tldavis09@yahoo.com>
2023-04-07 18:03:17 -0500

    WA: implement 'Sign' region component

--- application/web/source/src/view/landing/SignListRegion/SignRegion/SignRegion.jsx
+++ application/web/source/src/view/landing/SignListRegion/SignRegion/SignRegion.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function SignRegion() {
-	return <div>SignRegion</div>;
+function SignRegion({ sign }) {
+	return (
+		<div className="sign-region" data-sign={sign.id}>
+			{sign.name}
+		</div>
+	);
 }
 
 export default SignRegion;
