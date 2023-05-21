e4e34d0987c153bf67813266f2453728404aa619
tldav <tldavis09@yahoo.com>
2023-05-21 17:04:58 -0500

    IT: created from login view test for member landing view

--- application/web/test/src/test/java/com/tekgs/astro/view/memnberLanding/MemberLandingViewTests.java
+++ application/web/test/src/test/java/com/tekgs/astro/view/memnberLanding/MemberLandingViewTests.java
@@ -1,5 +1,8 @@
 package com.tekgs.astro.view.memnberLanding;
 
+import com.tekgs.astro.data.user.User;
+import com.tekgs.astro.data.user.UserProvider;
+import com.tekgs.astro.view.login.LoginView;
 import com.tekgs.astro.view.memberLanding.MemberLandingView;
 import com.tekgs.astro.view.memberLanding.MemberLandingViewCalibrator;
 import com.tekgs.astro.view.memberLanding.MemberLandingViewExpected;
@@ -8,14 +11,18 @@ import org.testng.annotations.Test;
 
 @Test(groups = {GauntletTest.Application.ASTRO_TEK, GauntletTest.View.MEMBER_LANDING})
 public class MemberLandingViewTests extends GauntletTest {
-    
     @Test(groups = {TestSuite.SMOKE, TestSuite.DEBUG})
     public void smoke() {
         MemberLandingViewExpected expected = MemberLandingViewExpected.getInstance();
         MemberLandingView actual = MemberLandingView.directNav();
         then(MemberLandingViewCalibrator.getInstance(expected, actual));
     }
+    
+    @Test(groups = {TestSuite.SMOKE, TestSuite.DEBUG})
+    public void fromLoginView() {
+        User user = UserProvider.getInstance().get();
+        MemberLandingViewExpected expected = MemberLandingViewExpected.getInstance();
+        MemberLandingView actual = LoginView.directNav().submitLogin(user);
+        then(MemberLandingViewCalibrator.getInstance(expected, actual));
+    }
 }
-
-
-
